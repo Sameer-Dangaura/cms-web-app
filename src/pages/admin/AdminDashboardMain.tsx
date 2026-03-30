422cb83d32fecaab8c23b7b113c94771946589bc
Sameer-Dangaura <sameerdangaura1@gmail.com>
2026-03-30 17:37:41 +0545

    refactor: remove div tag with main tag for SEO

--- src/pages/admin/AdminDashboardMain.tsx
+++ src/pages/admin/AdminDashboardMain.tsx
@@ -6,13 +6,13 @@ export default function AdminDashboardMain() {
         <>
             <div className="flex ">
                 <AdminSidebar />
-                <div className="w-full bg-gray-50">
+                <main className="w-full bg-gray-50">
                     <section>
 
                         <Outlet />   {/* This is where the nested routes will be rendered. For example, when you navigate to /admin it will render the Dashboard component. And when you navigate to /admin/users the UserList component will be rendered here. And when you navigate to /admin/products the ProductList component will be rendered here. This allows you to have a consistent layout (with the AdminSidebar) while changing the main content based on the route. */}
 
                     </section>
-                </div>
+                </main>
             </div>
         </>
     )
