bf1339e8ed1e3b216f9614ee644b99042a725232
Sameer-Dangaura <sameerdangaura1@gmail.com>
2026-03-30 17:11:16 +0545

    refactor: move aside from common to ui folder and update aside to sidebar

--- src/pages/admin/AdminDashboardMain.tsx
+++ src/pages/admin/AdminDashboardMain.tsx
@@ -1,15 +1,15 @@
 
 import { Outlet } from 'react-router';
-import Aside from '../../components/common/aside/Aside';
+import AdminSidebar from '../../components/ui/sidebar/AdminSidebar';
 export default function AdminDashboardMain() {
     return (
         <>
             <div className="flex ">
-                <Aside />
+                <AdminSidebar />
                 <div className="w-full bg-gray-50">
                     <section>
 
-                        <Outlet />   {/* This is where the nested routes will be rendered. For example, when you navigate to /admin it will render the Dashboard component. And when you navigate to /admin/users the UserList component will be rendered here. And when you navigate to /admin/products the ProductList component will be rendered here. This allows you to have a consistent layout (with the Aside) while changing the main content based on the route. */}
+                        <Outlet />   {/* This is where the nested routes will be rendered. For example, when you navigate to /admin it will render the Dashboard component. And when you navigate to /admin/users the UserList component will be rendered here. And when you navigate to /admin/products the ProductList component will be rendered here. This allows you to have a consistent layout (with the AdminSidebar) while changing the main content based on the route. */}
 
                     </section>
                 </div>
