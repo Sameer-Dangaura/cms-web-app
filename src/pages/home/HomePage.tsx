605ad76855923166e75a2c5af8eeb0507c6b067a
Sameer-Dangaura <sameerdangaura1@gmail.com>
2026-04-16 23:45:48 +0545

    feat: redirect authenticated users to role-based dashboard for HomePage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,6 +3,10 @@
 // Stateful Components
 // 
 
+import { useEffect } from "react";
+import { useAuth } from "../../lib/hooks/useAuth";
+import { useNavigate } from "react-router";
+
 import LeftSidePanel from "../../components/auth/LeftSidePanel";
 import RightSidePanel from "../../components/auth/RightSidePanel";
 
@@ -14,6 +18,18 @@ import RightSidePanel from "../../components/auth/RightSidePanel";
 
 // HomePage is a higher order component because it is composed of multiple lower order components like LeftSidePanel and RightSidePanel
 export default function HomePage() {
+
+    const { authUser } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        if (authUser && authUser.role) {
+            // i.e. logged in user
+            navigate('/' + authUser.role)   // navigate to the dashboard of the user based on their role (e.g., /admin for admin users, /user for regular users, etc.)
+        }
+
+    }, [authUser])  // this useEffect will run whenever the authUser state changes. If there is a logged-in user (i.e., authUser is not null) and they have a role defined, it will navigate to the corresponding dashboard based on their role. For example, if the user's role is "admin", it will navigate to "/admin". If the user's role is "user", it will navigate to "/user". This ensures that when a user logs in, they are automatically redirected to the appropriate dashboard based on their role.
 
     return (
         // do always return something from a component, even if it is null or empty div 
