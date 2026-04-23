2cc893666b83019dabb199be82e8b3316edb5ea2
Sameer-Dangaura <sameerdangaura1@gmail.com>
2026-04-23 16:58:59 +0545

    feat: add users sidebar access privilege for admin  in efficient way

--- src/components/ui/sidebar/AdminSidebar.tsx
+++ src/components/ui/sidebar/AdminSidebar.tsx
@@ -1,6 +1,7 @@
 import { LuLayoutDashboard, LuLogOut, LuMail, LuSettings, LuShoppingBag, LuUsers } from "react-icons/lu";
 import { NavLink } from "react-router";
 import type { IUserDetail } from "../../auth/Auth.contract";
+import ShowComponent from './../../auth/AllowAccess';
 
 
 export default function AdminSidebar({ loggedInUser }: Readonly<{ loggedInUser: IUserDetail }>) {
@@ -12,8 +13,15 @@ export default function AdminSidebar({ loggedInUser }: Readonly<{ loggedInUser: 
                         <ul>
                             <li>
                                 <div className="border-b-black-300/10 border-b pb-0.5">
+
+                                    <ShowComponent role="admin">
+                                        <NavLink to="/admin/users" className="flex items-center gap-3 hover:bg-gray-300 px-5 py-2" end>
+                                            <LuUsers />
+                                            <span>Users</span>
+                                        </NavLink>
+                                    </ShowComponent>
+
                                     {
-                                        // In a real application, you would get the logged in user information from your authentication context or state management. For this example, we will hardcode a logged in user object. And we will check the role of the logged in user. If the role is admin then we will show the dashboard link in the sidebar. Otherwise, we will not show the dashboard link. 
                                         // (loggedInUser) && (loggedInUser.role === 'admin') means that if the logged in user exists and the role of the logged in user is admin then we will render the dashboard link. Otherwise, we will render an empty fragment. This is a common way to conditionally render components in React based on the user's role or permissions.
                                         (loggedInUser) && (loggedInUser.role === 'admin') ? <NavLink to="/admin" className="flex items-center gap-3 hover:bg-gray-300 px-5 py-2" end>
                                             <LuLayoutDashboard />
@@ -31,11 +39,6 @@ export default function AdminSidebar({ loggedInUser }: Readonly<{ loggedInUser: 
                                     <NavLink to="/orders" className="flex items-center gap-3 hover:bg-gray-300 px-5 py-2" end>
                                         <LuMail />
                                         <span>Orders</span>
-                                    </NavLink>
-
-                                    <NavLink to="/admin/users" className="flex items-center gap-3 hover:bg-gray-300 px-5 py-2" end>
-                                        <LuUsers />
-                                        <span>Users</span>
                                     </NavLink>
                                 </div>
                                 <div className="pt-0.5">
